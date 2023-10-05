bd9840f91f47cfe679456e1f7543873fca2d3f60
Creationdis <creationdis@gmail.com>
2023-10-05 06:42:17 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,50 @@
 #!/usr/bin/python3
-#  Fabric script that generates a .tgz archive from the
-# contents of the web_static folder of your AirBnB Clone repo
-# using the function do_pack
+""" module doc
+"""
+from fabric.api import task, local, env, put, run
+from datetime import datetime
 import os
-from fabric.api import run, put, env
 
-env.hosts = ['52.207.208.21', '54.146.74.86']
-env.user = "ubuntu"
+env.hosts = ['52.207.208.21', '52.206.189.175']
 
 
+@task
+def do_pack():
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
+
+
+@task
 def do_deploy(archive_path):
-    """Create a tar gzipped archive of the directory web_static."""
-    if os.path.exists(archive_path) is False:
-        return False
-    else:
-        try:
-            put(archive_path, "/tmp/")
-            """ putting the file to .tgz """
-            file_name = archive_path.split("/")[1]
-            """ splitting .tgz """
-            file_name2 = file_name.split(".")[0]
-            """ spliting archivo """
-            final_name = "/data/web_static/releases/" + file_name2 + "/"
-            run("mkdir -p " + final_name)
-            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
-            run("rm /tmp/" + file_name)
-            run("mv " + final_name + "web_static/* " + final_name)
-            run("rm -rf " + final_name + "web_static")
-            run("rm -rf /data/web_static/current")
-            run("ln -s " + final_name + " /data/web_static/current")
-            print("New version deployed!")
-            return True
-        except Exception:
+    """ method doc
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
+    """
+    try:
+        if not os.path.exists(archive_path):
             return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
