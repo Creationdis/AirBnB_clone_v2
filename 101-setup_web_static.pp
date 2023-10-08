352a2c98b47180a380a33d52d566399ba1bc3104
creationdis <creationdis@gmail.com>
2023-10-08 09:06:37 -0700

    task5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,29 +1,29 @@
-# instal and configure nginx
-exec {'update':
-  command => '/usr/bin/apt-get update',
+# puppet to config the server
+exec { 'apt-get-update':
+  command => '/usr/bin/apt -y update',
 }
--> package { 'nginx':
+package { 'nginx':
   ensure => installed,
 }
--> exec { 'run1':
-  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
+exec { '/data/web_static/releases/test/':
+  command => '/usr/bin/mkdir -p /data/web_static/releases/test/',
 }
--> exec { 'run2':
-  command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
+exec { '/data/web_static/shared/':
+  command => '/usr/bin/mkdir -p /data/web_static/shared/',
 }
--> exec { 'run3':
-  command => '/usr/bin/rm -rf /data/web_static/current',
+exec { '/data/web_static/releases/test/index.html':
+  command => '/usr/bin/echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo /usr/bin/tee /data/web_static/releases/test/index.html',
 }
--> exec { 'run4':
-  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
+exec { 'update server':
+  command => '/usr/bin/ln -sf /data/web_static/releases/test/ /data/web_static/current',
 }
--> exec { 'run5':
+exec { 'chown -R ubuntu:ubuntu /data':
   command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
 }
--> exec { 'hbnb_static':
-  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}" /etc/nginx/sites-enabled/default',
+exec { '/etc/nginx/sites-available/default':
+  command => 'sudo sed -i "/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default',
   provider => shell,
 }
--> exec { 'run6':
-  command => '/usr/sbin/service nginx restart',
+exec { 'start-nginx':
+  command => '/usr/bin/sudo service nginx restart',
 }
